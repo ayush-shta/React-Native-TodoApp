db88f31f062da77ca556d6a14ffa3d69fc36cccd
Polymorpher01 <ayushshta@gmail.com>
2019-05-02 14:12:10 +0545

    Add snapshot tests for components, Move Realm Tests to the App tests file

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,11 +3,92 @@
  */
 
 import 'react-native';
+import React from 'react';
 
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+import TodoService from '../src/realm/service/todoService'
+import AddTodo from '../src/components/AddTodo';
+import CheckBox from '../src/components/CheckBox';
+import TodoListItem from '../src/components/TodoListItem';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import { MenuProvider } from 'react-native-popup-menu';
+
+function initalizeDatabase() {
+    const tasks = ["one", "two", "three"];
+    TodoService.saveMultiple(tasks);
+}
+
+function clearDatabase() {
+    TodoService.deleteAll();
+}
+
+beforeAll(() => {
+    return initalizeDatabase();
 });
 
-test('but there is a "stop" in Christoph', () => {
-    expect('Christoph').toMatch(/stop/);
+
+afterAll(() => {
+    return clearDatabase();
+})
+
+describe('Realm Tasks', () => {
+    test('Todo list exists', () => {
+        const todoList = TodoService.findall();
+        expect(todoList).toBeTruthy();
+    });
+
+    test('Task one exists', () => {
+        const id = 1;
+        const todo = TodoService.findById(id);
+        expect(todo.task).toBe("one");
+    })
+
+    test('Task updates', () => {
+        const id = 1;
+        const todo = TodoService.findById(id);
+        TodoService.update(() => {
+            todo.task = "one updated";
+        })
+        expect(todo.task).toBe("one updated");
+    })
+
+    test('Task is deleted', () => {
+        const id = 1;
+        const todo = TodoService.findById(id);
+        TodoService.delete(todo);
+        const todoDeleted = TodoService.findById(id);
+        expect(todoDeleted).toBeFalsy();
+    })
 });
+
+describe('Component renders correctly', () => {
+
+    it('AddTodo', () => {
+        const instance = renderer.create(
+            <AddTodo />);
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    it('CheckBox', () => {
+        const todo = TodoService.findById(2);
+        const instance = renderer.create(
+            <CheckBox
+                data={todo}
+            />);
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+
+    it('Todo List Item', () => {
+        const todo = TodoService.findById(2);
+        const instance = renderer.create(
+            //Menu Component inside TodoListItem requires a MenuProvider as root component
+            <MenuProvider>
+                <TodoListItem
+                    todo={todo}
+                />
+            </MenuProvider>
+        );
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
+})
